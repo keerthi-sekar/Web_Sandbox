362011db3f8dd1461e8c90ca144054c73dd5b378
Keerth Sekar <keerthi.sekar101@gmail.com>
2022-05-21 10:55:57 -0700

    Create DragObject
    TODO: test with mouse lol

---
+++ Sandbox/Assets/Scripts/DragObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToMove = null;
+    [SerializeField]
+    private Camera cameraToReference = null;
+
+    private Vector3 offset;
+    private float ZCoord;
+
+    private void OnMouseDown()
+    {
+        ZCoord = cameraToReference.WorldToScreenPoint(objectToMove.transform.position).z;
+        offset = objectToMove.transform.position - GetMouseOffset();
+    }
+
+    private Vector3 GetMouseOffset()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = ZCoord;
+
+        return cameraToReference.WorldToScreenPoint(mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        objectToMove.transform.position = offset + GetMouseOffset();
+    }
+}
