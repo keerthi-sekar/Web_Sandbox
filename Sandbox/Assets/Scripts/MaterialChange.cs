00a8f374d3b5cfeb6feac9a97d03f45f2fac7bc5
Keerth Sekar <keerthi.sekar101@gmail.com>
2022-05-21 10:31:15 -0700

    material change for cubes

---
+++ Sandbox/Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+
+    [SerializeField]
+    private MeshRenderer materialToChange = null;
+
+    [SerializeField]
+    private Material oldMaterial = null;
+
+    [SerializeField]
+    private Material newMaterial = null;
+
+    private void OnMouseDown()
+    {
+        materialToChange.material = newMaterial;
+    }
+
+    private void OnMouseUp()
+    {
+        materialToChange.material = oldMaterial;
+    }
+}
