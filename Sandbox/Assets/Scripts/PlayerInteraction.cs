db789e0fd092d164e5aa6c45043117221e3b8f3c
Keerth Sekar <keerthi.sekar101@gmail.com>
2022-05-21 10:19:48 -0700

    Create Touch Targets for Cylinders

---
+++ Sandbox/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public event Action<bool> OnGrab;
+    public event Action<bool> OnHover; //For Highlight on Hover - use separate class with OnTriggerEnter, OnTriggerStay, OnTriggerExit (Colliders), and OnMouseDown
+
+    private void OnMouseDown()
+    {
+        if (OnGrab != null)
+        {
+            OnGrab(true);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (OnGrab != null)
+        {
+            OnGrab(false);
+        }
+    }
+
+}
---
+++ Sandbox/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerCamera = null;
+    [SerializeField]
+    private PlayerInteraction playerInteraction = null;
+    [SerializeField]
+    private Transform destination = null;
+    [SerializeField]
+    private AnimationCurve animationCurve = null;
+
+    private Vector3 currentPosition;
+    private Quaternion currentRotation;
+    private bool moving = false;
+    private float time = 0.0f;
+
+    protected void OnEnable()
+    {
+        playerInteraction.OnGrab += Travel;
+    }
+
+    protected void OnDisable()
+    {
+        playerInteraction.OnGrab -= Travel;
+    }
+
+    protected void LateUpdate()
+    {
+        if (!moving)
+        {
+            return;
+        }
+        playerCamera.position = Vector3.Slerp(currentPosition, destination.position, animationCurve.Evaluate(time));
+        playerCamera.rotation = Quaternion.Slerp(currentRotation, destination.rotation, animationCurve.Evaluate(time));
+        if (time <= animationCurve.keys[animationCurve.length - 1].time)
+        {
+            moving = true;
+            time += Time.deltaTime;
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+    private void Travel(bool value)
+    {
+        if (!value)
+        {
+            return;
+        }
+        moving = true;
+        currentPosition = playerCamera.position;
+        currentRotation = playerCamera.rotation;
+        time = 0;
+    }
+}
